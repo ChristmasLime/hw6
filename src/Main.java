5864022cacdf9563ef5eab17eb98acf52b7c194a
ChristmasLime <christmasboom@mail.ru>
2023-02-13 17:37:01 +0300

    Задача № 4

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,9 @@ public class Main {
     }
     public static void task4() {
         System.out.println("Задача 4");
-
+        for ( int i = 10; i >=-10;--i) {
+            System.out.println(i);
+        }
     }
     public static void task5() {
         System.out.println("Задача 5");
